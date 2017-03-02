c066027c969473d1e82b9de6dc3ef6e7e136bce9
toipacoelho <toipacoelho@gmail.com>
2017-03-02 12:05:15 +0000

    .gitignore added

--- P1/P1.sim/sim_1/behav/elaborate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=D:\\Programs\\Vivado\\Vivado\\2016.4\\bin
-call %xv_path%/xelab  -wto 929f8b71ae234e14bf9678e4099806fe -m64 --debug typical --relax --mt 2 -L xil_defaultlib -L secureip --snapshot SDR_P1_behav xil_defaultlib.SDR_P1 -log elaborate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
--- README.md
+++
@@ -1,3 +0,0 @@
-# cr
-Reconfigurable Computation course on Computer and Telematics Engineering course. <br>
-This project was created using Vivado 2016.4.
