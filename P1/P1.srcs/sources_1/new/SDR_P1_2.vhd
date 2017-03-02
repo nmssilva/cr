9ae39ee5775757bca8669106fc7d70d17600921b
toipacoelho <toipacoelho@gmail.com>
2017-03-02 12:07:16 +0000

    .gitignore updated to include readme 2

---
+++ P1/P1.srcs/sources_1/new/SDR_P1_2.vhd
@@ -0,0 +1,104 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 27.02.2017 15:46:14
+-- Design Name: 
+-- Module Name: SDR_P1 - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity SDR_P1 is
+  Port (sw      : in    std_logic_vector(15 downto 0);
+        btnD    : in    std_logic;
+        btnU    : in    std_logic;
+        btnL    : in    std_logic;
+        btnR    : in    std_logic;
+        btnC    : in    std_logic;
+        led     : out   std_logic_vector(15 downto 0));
+        
+end SDR_P1;
+
+architecture Behavioral of SDR_P1 is
+
+begin
+my_case : process(sw)
+    begin
+    
+    case sw(3 downto 0) is
+    
+        when "0000" => 
+            led(0) <= btnr;
+            
+        when "0001" => 
+            led(0) <= btnd;
+ 
+        when "0010" => 
+            led(0) <= btnc;
+                
+        when "0011" => 
+            led(0) <= btnu;
+            
+        when "0100" => 
+            led(0) <= btnl;
+            
+        when "0101" => 
+            led(0) <= sw(5);
+ 
+        when "0110" => 
+            led(0) <= sw(6);
+                
+        when "0111" => 
+            led(0) <= sw(7);            
+                    
+        when "1000" => 
+            led(0) <= sw(8);
+            
+        when "1001" => 
+            led(0) <= sw(9);
+ 
+        when "1010" => 
+            led(0) <= sw(10);
+                
+        when "1011" => 
+            led(0) <= sw(11);
+            
+        when "1100" => 
+            led(0) <= sw(12);
+            
+        when "1101" => 
+            led(0) <= sw(13);
+ 
+        when "1110" => 
+            led(0) <= sw(14);
+            
+        -- 1111
+        when others => 
+            led(0) <= sw(15);
+    end case;
+
+end process my_case;
+end Behavioral;
