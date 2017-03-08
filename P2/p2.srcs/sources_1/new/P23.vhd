38e8feff9e7fc9d8b6c7629ebed6e05b2598a3c2
toipacoelho <toipacoelho@gmail.com>
2017-03-08 23:32:17 +0000

    P2 exercise 3 done

--- P2/p2.srcs/sources_1/new/P23.vhd
+++ P2/p2.srcs/sources_1/new/P23.vhd
@@ -37,7 +37,7 @@ entity P2 is
     generic( how_fast : integer :=30);
     Port(led    :   out std_logic_vector(15 downto 0);
          sw     :   in std_logic_vector(1 downto 0);
-         clk, btnC, btnU, btnR : in std_logic); -- btnC as reset
+         clk, btnC, btnU, btnR, btnL : in std_logic); -- btnC as reset
 end P2;
 
 architecture Behavioral of P2 is
@@ -46,6 +46,7 @@ architecture Behavioral of P2 is
     signal right            :std_logic := btnU;
     signal positive_reset   :std_logic := btnC;
     signal load_enable      :std_logic := btnR;
+    signal clock_enable     :std_logic := btnL;
     signal shift            :std_logic_vector(14 downto 0);
 begin
 
@@ -62,16 +63,18 @@ end process sp1;
 
 sp3: process (divided_clk)
 begin
-    if rising_edge(divided_clk) then
-        if positive_reset ='1' then shift<=(others=>'0');
-        else
-            if load_enable ='1' then shift <=sw;
-            elsif right = '1' then
-                shift <= shift(0) & shift(14 downto 1);
+    if clock_enable ='1' then
+        if rising_edge(divided_clk) then
+            if positive_reset ='1' then shift<=(others=>'0');
             else
-                shift <= shift(13 downto 0) & shift(14);
+                if load_enable ='1' then shift <=sw;
+                elsif right = '1' then
+                    shift <= shift(2 downto 0) & shift(14 downto 2);
+                else
+                    shift <= shift(12 downto 0) & shift(14 downto 13);
+                end if;
             end if;
-         end if;
+        end if;
     end if;
 end process sp3;
 
